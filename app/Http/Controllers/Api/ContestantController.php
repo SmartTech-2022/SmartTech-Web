724259b743afb7fd1e7765f3839eaae4d67ed65a
icode/ <ucesty5@gmail.com>
2023-01-10 16:07:22 +0100

    added api controllers and some models

---
+++ app/Http/Controllers/Api/ContestantController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Contestant;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ContestantController extends Controller
+{
+
+        public function show($id)
+        {
+            $contestant = Contestant::findOrFail($id);
+            return view('contestants.show', ['contestant' => $contestant]);
+        }
+    }
+
+
