a8c0941ed140fa7162ffd83c584a450a0ed722a2
SmartTech <smarttech@ictyepprojects.com>
2023-01-11 13:12:52 +0100

    Chore: Added admin panel views

---
+++ resources/views/contestants/create.blade.php
@@ -0,0 +1,48 @@
+
+@section('content')
+
+<div class="container my-5 py-5">
+ @if (session()->has('message'))
+ <div class="alert alert-success">
+     {{ session()->get('message') }}
+ </div>
+ @endif
+
+
+ <h5>   Add a Contestant   </h5>
+
+     <form method="POST" action="{{ route('contestant.store') }}" class="border py-3 px-3 rounded" enctype="multipart/form-data">
+
+         <div class="mb-3">
+             @csrf
+             <input type="text" name="name" class="form-control  @error('name') is-invalid @enderror" id="name" placeholder="name" value="">
+             @error('name')
+                 <small class="text-danger"> {{ $message }}</small>
+             @enderror
+         </div>
+         <hr>
+         <div class="mb-3">
+
+             <input type="text" name="about" class="form-control @error('about') is-invalid @enderror"  id="about" placeholder="about" value="">
+             @error('about')
+                 <small class="text-danger"> {{ $message }}</small>
+             @enderror
+         </div>
+         <hr>
+         <div class="mb-3">
+
+             <input class="form-control @error('electionId') is-invalid @enderror" name="electionId"  id="electionId"placeholder="electionId">
+
+             @error('electionId')
+                 <small class="text-danger"> {{ $message }}</small>
+             @enderror
+         </div>
+
+
+         <hr>
+         <button type="submit" class="btn btn-outline-info w-30 btn-lg">Submit</button><br><br>
+
+     </form>
+ </div>
+@endsection
+
