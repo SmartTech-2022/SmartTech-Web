b9f632d1d8eb3e7c983b0549d3c4d16f561e6066
icode/ <ucesty5@gmail.com>
2023-01-20 12:06:03 +0100

    #

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\Api\ContestantController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::post('login', [AuthController::class, 'loginUser']);
+Route::post('login', [AuthController::class, 'login']);
 // Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
