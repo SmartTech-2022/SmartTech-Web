a57b0e69128b6c1e6cd46d1fd9d9d496e1123df3
icode/ <ucesty5@gmail.com>
2023-01-20 10:39:01 +0100

    login fixed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -27,10 +27,49 @@ class AuthController extends Controller
         if(!Hash::check($request->input('password'), $user->password)){
             return response()->json(['message' => 'Voter id does not exist'], 401);
         }
-        
+
         Auth::login($user);
 
         return response()->json(["message" => "Login Successful", "user" => auth()->user(), 'token' => auth()->user()->createToken(config('app.key'))->plainTextToken,]);
+    }
+
+    public function loginUser(Request $request) {
+        try {
+            //code...
+            $validateUser = Validator::make($request->all(),
+                [
+                    'voter_id' => 'required|string',
+                    'password' => 'required'
+                ]);
+
+                if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation Error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+                }
+
+                if(!Auth::attempt($request->only(['voter_id', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Voter ID or Password does not match with our record.'
+                ], 401);
+                }
+
+            $user = User::where('voter_id', $request->voter_id)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -26,13 +26,13 @@ use App\Http\Controllers\Api\ContestantController;
 // });
 Route::post('login', [AuthController::class, 'loginUser']);
 // Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::post('register', [AuthController::class, 'register']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
-// Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
 
 
     // Election
@@ -45,18 +45,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::name('contestants.')->group(function () {
         Route::get('contestants/{id}', [ContestantController::class, 'show'])->name('show');
     });
-// });
 
-//     // Election
-//     Route::get('elections', [ElectionController::class, 'elections']);
+    Route::get('elections', [ElectionController::class, 'elections']);
 
 //     // contestant
-//     Route::get('contestants', [ElectionController::class, 'contestants']);
-//     Route::post('store', [VotesController::class, 'store']);
-//     Route::name('contestants.')->group(function () {
-//     Route::get('contestants/{id}', 'ContestantController@show')->name('show');
-//     });
-// });
+    Route::get('contestants', [ElectionController::class, 'contestants']);
+    Route::post('store', [VotesController::class, 'store']);
+    Route::name('contestants.')->group(function () {
+    Route::get('contestants/{id}', 'ContestantController@show')->name('show');
+    });
+});
 
 
 
